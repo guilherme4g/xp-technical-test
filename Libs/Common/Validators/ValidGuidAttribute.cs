e244c159be1d305b17c8db191ad7d342d40ab3f0
José Guilherme <jgnvs6@gmail.com>
2023-08-04 23:26:46 -0300

    feat: libs commons by services

---
+++ Libs/Common/Validators/ValidGuidAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.Validators
+{
+    public class ValidGuidAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var stringValue = value as string;
+
+            if (!string.IsNullOrEmpty(stringValue))
+            {
+                Guid guidValue;
+                return Guid.TryParse(stringValue, out guidValue);
+            }
+
+            return true;
+        }
+    }
+}
